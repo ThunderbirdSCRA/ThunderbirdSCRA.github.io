3e03add2e348477cabe7f99541481f5aea60fe62
ThunderbirdSCRA <thunderbirdscra@gmail.com>
2026-09-07 19:09:48 -0500

    Update Merch Photo

--- src/data/site.ts
+++ src/data/site.ts
@@ -356,7 +356,7 @@ export const MERCH = {
   name: "Thunderbird Chapter T-Shirt",
   price: "$25",
   checkoutUrl: "https://www.paypal.com/ncp/payment/HTK6MGQ3L5JP2",
-  photo: shirtAsset.url,
+  photo: "@/assets/back_medium.png",
   description:
     "Show your Thunderbird pride. These shirts are from a limited bulk order and are available to anyone who wants them. We prefer to coordinate local pickup when possible to avoid shipping fees. All proceeds support the chapter.",
 };
